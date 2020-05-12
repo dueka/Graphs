3674b2e54b2421917e1ad8f532e80af803f76541
dueka <ekanemd@live.com>
2020-05-12 21:57:37 +0100

    tests not passing

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,49 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, ancestor_id):
+        if ancestor_id not in self.vertices:
+            return
+        self.vertices[ancestor_id] = set()
+
+    def add_edge(self, children, parents):
+        if parents not in self.vertices[children] and parents in self.vertices and children in self.vertices:
+            self.vertices[children].add(parents)
+        else:
+            raise IndexError("Vertex does not exist!")
+
+    def get_ancestors(self, ancestor_id):
+        return self.vertices[ancestor_id]
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    # instantiate a BFT
+    q = Queue()
+    q.enqueue([starting_node])
+    max_length = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        last_vertex = path[-1]
+        if (len(path)) > max_length and last_vertex < earliest_ancestor:
+            earliest_ancestor = last_vertex
